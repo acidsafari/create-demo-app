019dcbb544aa862604b2270614d8f0f448bb0ca7
Samuel Klett Navarro <samuelklettnavarro@gmail.com>
2024-04-22 16:12:49 +1000

    basic test (#9)
    basic test from w8 notes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
+import React from 'react'; 
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './App.js';
+//COS60015_FRONT-END/create-demo-app/src/App.js
 
-test('renders learn react link', () => {
+test('renders page title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("Creative Demonstrations app");
   expect(linkElement).toBeInTheDocument();
 });
